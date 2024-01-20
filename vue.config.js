36e4295485ba3c378307c340e35a474d8e39baa5
unknown <srikanthcollaty92@gmail.com>
2024-01-20 18:41:48 +0100

    'deployment'

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+    publicPath:'/srikanthsc.vuejsportfolio/',
+  
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
